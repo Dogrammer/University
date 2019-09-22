c3fac10baa3391e2b24f411cd6f6cb746382bea3
Dogrammer <marjanovicnevio@gmail.com>
2019-09-22 13:17:50 +0200

    Initialize File System

---
+++ College.Model/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace College.Model
+{
+    public class Class1
+    {
+    }
+}
---
+++ College.Shared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace College.Shared
+{
+    public class Class1
+    {
+    }
+}
